53985a8868d146a6baf10b90441a510f5b85841b
NOPe <arvipad002@gmail.com>
2021-02-20 07:25:43 +0530

    first commit

---
+++ Quick start Bar/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Quick_start_Bar
+{
+	partial class Start
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Start));
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button3 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.Text = "notifyIcon1";
+			this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick_1);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+			this.button2.BackgroundImage = global::Quick_start_Bar.Properties.Resources.Exit;
+			this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Location = new System.Drawing.Point(251, 0);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(34, 19);
+			this.button2.TabIndex = 1;
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button1
+			// 
+			this.button1.BackgroundImage = global::Quick_start_Bar.Properties.Resources.Minimise2;
+			this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Location = new System.Drawing.Point(211, 0);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(34, 19);
+			this.button1.TabIndex = 0;
+			this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("JetBrains Mono", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.Pink;
+			this.label1.Location = new System.Drawing.Point(61, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(144, 19);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Quick Start Bar";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(12, 352);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// Start
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.YellowGreen;
+			this.ClientSize = new System.Drawing.Size(284, 387);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.button2);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Start";
+			this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button3;
+	}
+}
+
---
+++ Quick start Bar/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quick_start_Bar
+{
+	public partial class Start : Form
+	{
+		int height = SystemInformation.VirtualScreen.Height;
+		int width = SystemInformation.VirtualScreen.Width;
+		public Start()
+		{
+			InitializeComponent();
+			this.StartPosition = FormStartPosition.Manual;
+			this.Location = new Point(width - 284, 0);
+			this.Size = new Size(284, width);				
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Hide();
+			notifyIcon1.Visible = true;
+			
+		}
+
+		private void notifyIcon1_MouseDoubleClick_1(object sender, MouseEventArgs e)
+		{
+			Show();
+			this.WindowState = FormWindowState.Normal;
+			notifyIcon1.Visible = false;
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			this.Visible = false;
+			this.Location = new Point(width - 50, height - 850);
+		}
+	}
+}
